edeedb446617819ed0d0b28aaabc1ed42da42f86
danielmu22 <56256124+danielmu22@users.noreply.github.com>
2023-02-22 17:27:26 +0100

    Añadida función modificar juego

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import os
 from gestion_juegos import *
 
-    
-def menu_opciones ():
-   
+
+def menu_opciones():
+
     permanecer = True
-    
+
     while permanecer:
         mostrar_opciones()
         try:
-            eleccion = int(input("Seleciona la característica a la que quieres acceder: "))
+            eleccion = int(
+                input("Seleciona la característica a la que quieres acceder: "))
         except:
             print("Solo acepto las opciones que te di")
-             
+
         try:
             match eleccion:
                 case 1:
                     os.system('clear')
                     alta_juego_input()
+                case 2:
+                    os.system('clear')
+
+                case 3:
+                    os.system('clear')
+
+                case 4:
+                    os.system('clear')
+
                 case 13:
                     os.system('clear')
                     print("Adios")
@@ -28,13 +38,16 @@ def menu_opciones ():
         except:
             os.system('clear')
             print("No se que has seleccionado, solo acepto las siguientes opciones:")
-            
-            
+
+
 def mostrar_opciones():
     print("Menu")
     print(" 1 - Insertar un videojuego")
+    print(" 2 - Borrar un videojuego")
+    print(" 3 - Modificar un videojuego")
+    print(" 4 - Listar los videojuegos")
     print(" 13 - Salir del programa")
     print("--------------------------------")
-        
+
 
 menu_opciones()
