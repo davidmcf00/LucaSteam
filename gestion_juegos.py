0a7837395b38410112e84b25280015d803659a14
danielmu22 <56256124+danielmu22@users.noreply.github.com>
2023-02-23 17:56:43 +0100

    Todo el código implementado

--- gestion_juegos.py
+++ gestion_juegos.py
@@ -1,5 +1,9 @@
 import csv
 from typing import Any, Hashable, Iterable, Optional
+import unittest
+from unittest.mock import patch
+import pandas as pd
+import numpy as np
 
 
 def convert_csv():
@@ -40,19 +44,21 @@ def alta_juego(name, platform, year, genre, publisher, na_sales, eu_sales, jp_sa
                   "JP_Sales": jp_sales,
                   "Other_Sales": other_sales,
                   "Global_Sales": global_sales})
+    registro = buscar_dicc(lista, "Name", name)
+    mostrar_registro(registro)
 
 
 def alta_juego_input():
-    Name = input("Introduce el onmbre del juego que quieres dar de alta:")
-    Platform = input("Introduce la plataforma:")
-    Year = input("Introduce el año: ")
-    Genre = input("Introduce el genero:")
-    Publisher = input("Introduce el editor:")
-    NA_Sales = input("Introduce las ventas en América ")
-    EU_Sales = input("Introduce las ventas en Europa:")
-    JP_Sales = input("Introduce las ventas en Japón:")
-    Other_Sales = input("Introduce otras ventas:")
-    Global_Sales = input("Introduce las ventas globales:")
+    Name = input("Escribe el nombre del juego que quieres dar de alta:")
+    Platform = input("Escribe la plataforma:")
+    Year = input("Escribe el año: ")
+    Genre = elegir_genero()
+    Publisher = input("Escribe el editor:")
+    NA_Sales = input("Escribe las ventas en América ")
+    EU_Sales = input("Escribe las ventas en Europa:")
+    JP_Sales = input("Escribe las ventas en Japón:")
+    Other_Sales = input("Escribe otras ventas:")
+    Global_Sales = input("Escribe las ventas globales:")
     alta_juego(Name, Platform, Year, Genre, Publisher, NA_Sales,
                EU_Sales, JP_Sales, Other_Sales, Global_Sales)
 
@@ -61,6 +67,7 @@ def modificar_juego(registro, Name, Platform, Year, Genre, Publisher, NA_Sales,
                     EU_Sales, JP_Sales, Other_Sales, Global_Sales):
     registro.update({"Name": Name, "Platform": Platform, "Year": Year, "Genre": Genre, "Publisher": Publisher, "NA_Sales": NA_Sales,
                     "EU_Sales": EU_Sales, "JP_Sales": JP_Sales, "Other_Sales": Other_Sales, "Global_Sales": Global_Sales})
+    mostrar_registro(registro)
 
 
 def modificar_juego_input():
@@ -68,7 +75,7 @@ def modificar_juego_input():
     registro = buscar_dicc(lista, "Name", nombre_juego)
 
     print("Si presionas enter sin nada, se dejara el valor por defecto")
-    print(registro)
+    mostrar_registro(registro)
 
     nuevo_nombre_juego = input("Dime el nuevo NOMBRE del juego: ")
     if (nuevo_nombre_juego == ""):
@@ -82,7 +89,7 @@ def modificar_juego_input():
     if (nuevo_ano_juego == ""):
         nuevo_ano_juego = registro.get("Year")
 
-    nuevo_genero_juego = input("Dime el nuevo GENERO del juego: ")
+    nuevo_genero_juego = elegir_genero()
     if (nuevo_genero_juego == ""):
         nuevo_genero_juego = registro.get("Genre")
 
@@ -118,7 +125,7 @@ def modificar_juego_input():
 
 def listar_juego():
     print("| Rank | Name | Platform | Year | Genre | Publisher | NA Sales | EU Sales | JP Sales | Other Sales | Global Sales |")
-    limite = 10
+    limite = 11
     contador = 0
     for elemento in lista:
         contador += 1
@@ -147,5 +154,190 @@ def borrar_juego():
 
 def buscar_por_nombre():
     Name = input("Selecciona el juego que quieres buscar:")
-    objeto = buscar_dicc(lista, "Name", Name)
-    print(objeto)
+    registro = buscar_dicc(lista, "Name", Name)
+    mostrar_registro(registro)
+
+
+def mostrar_registro(elemento):
+    print()
+    print(" | Name | Platform | Year | Genre | Publisher | NA Sales | EU Sales | JP Sales | Other Sales | Global Sales |")
+    print(" | ", elemento["Name"], " | ",
+          elemento["Platform"], " | ",
+          elemento["Year"], " | ",
+          elemento["Genre"], " | ",
+          elemento["Publisher"], " | ",
+          elemento["NA_Sales"], " | ",
+          elemento["EU_Sales"], " | ",
+          elemento["JP_Sales"], " | ",
+          elemento["Other_Sales"], " | ",
+          elemento["Global_Sales"], " | ")
+    print("--------------------------------------------------------------------------------------------------------------------------------------------------------")
+
+
+def elegir_genero():
+    """
+    generos = ["Sports", "Platform", "Racing", "Role-playing", "Misc",
+               "Shooter", "Simulation", "Action", "Puzzle", "Fighting", "Adventure"]
+    """
+    generos = []
+
+    for registro in lista:
+        generos.append(registro["Genre"])
+
+    generos = list(set(generos))
+
+    print("Elige uno de los siguientes géneros")
+    for i in range(len(generos)):
+        print(i, "-", generos[i])
+    try:
+        genero_juego = int(
+            input("¿De que género es tu juego, escribe el número? "))
+    except:
+        print("Pon el número, porfa")
+
+    return generos[genero_juego]
+
+
+def mas_vendidos_mundo():
+    limite = 5
+    contador = 0
+    i = 0
+    while i < len(lista) and contador < limite:
+        elemento = lista[i]
+        contador += 1
+        i += 1
+        registro = pd.DataFrame(np.array([[elemento["Rank"], elemento["Name"], elemento["Platform"], elemento["Year"],
+                                           elemento["Genre"], elemento["Publisher"], elemento["NA_Sales"],
+                                           elemento["EU_Sales"], elemento["JP_Sales"], elemento["Other_Sales"],
+                                           elemento["Global_Sales"]]]),
+                                columns=["Rank", "Name", "Platform", "Year", "Genre", "Publisher",
+                                         "NA_Sales", "EU_Sales", "JP_Sales", "Other_Sales", "Global_Sales"])
+        print(registro)
+
+
+def juegos_siglo():
+    registros_siglo = []
+    for registro in lista:
+        year = registro["Year"]
+        if year.isdigit() and int(year) >= 1901 and int(year) <= 2000:
+            registros_siglo.append(registro)
+    lista_juego_2(registros_siglo)
+
+
+def lista_juegos_genero_input():
+    genero = elegir_genero()
+    lista_juegos_genero(genero)
+
+
+def lista_juegos_genero(genero):
+    lista_juegos = buscar_varios_dicc(lista, "Genre", genero)
+    lista_juego_2(lista_juegos)
+
+
+def lista_juego_2(lista):
+    limite = 11
+    contador = 0
+
+    for elemento in lista:
+        contador += 1
+        if contador == limite:
+            break
+        registro = pd.DataFrame(np.array([[elemento["Rank"], elemento["Name"], elemento["Platform"], elemento["Year"],
+                                           elemento["Genre"], elemento["Publisher"], elemento["NA_Sales"],
+                                           elemento["EU_Sales"], elemento["JP_Sales"], elemento["Other_Sales"],
+                                           elemento["Global_Sales"]]]),
+                                columns=["Rank", "Name", "Platform", "Year", "Genre", "Publisher",
+                                         "NA_Sales", "EU_Sales", "JP_Sales", "Other_Sales", "Global_Sales"])
+        print(registro)
+
+
+def elegir_plataforma():
+
+    # plataformas = ["Wii", "WiiU" "N64", "NES", "GB", "GBA", "SNES", "X360", "DS", "SNES", "PS", "PS2", "PS3", "PS4", "3DS", "XOne", "GC", "PC", "GEN"]
+
+    plataformas = []
+
+    for registro in lista:
+        plataformas.append(registro["Platform"])
+
+    plataformas = list(set(plataformas))
+
+    print("Elige una de las siguientes plataformas para buscar todos sus juegos")
+    for i in range(len(plataformas)):
+        print(i, "-", plataformas[i])
+    try:
+        genero_juego = int(
+            input("¿De que plataforma es tu juego, escribe el número? "))
+    except:
+        print("Pon el número, porfa")
+
+    return plataformas[genero_juego]
+
+
+def lista_juegos_plataforma_input():
+    plataforma = elegir_plataforma()
+    print("Has elegido los juegos para: ", plataforma)
+    print("--------------------------------------")
+    lista_juegos_plataforma(plataforma)
+
+
+def lista_juegos_plataforma(plataforma):
+    lista_juegos = buscar_varios_dicc(lista, "Platform", plataforma)
+    lista_juego_2(lista_juegos)
+
+
+def elegir_editores():
+    editores = ["Nintendo", "Microsoft Game Studios", "Take-Two Interactive", "Sony Computer Entertainment",
+                "Activision", "Bethesda Softworks", "Electronic Arts", "Sega", "SquareSoft", "Atari",
+                "Ubisoft", "Capcom", "Konami Digital Entertainment", "LucasArts", "505 Games",
+                "Virgin Interactive", "Warner Bros"]
+
+
+def elegir_editores():
+
+    # plataformas = ["Wii", "WiiU" "N64", "NES", "GB", "GBA", "SNES", "X360", "DS", "SNES", "PS", "PS2", "PS3", "PS4", "3DS", "XOne", "GC", "PC", "GEN"]
+
+    editores = []
+
+    for registro in lista:
+        editores.append(registro["Publisher"])
+
+    editores = list(set(editores))
+
+    print("Elige una de las siguientes empresas para buscar todos sus juegos")
+    for i in range(len(editores)):
+        print(i, "-", editores[i])
+    try:
+        genero_juego = int(
+            input("¿De que empresa es tu juego, escribe el número? "))
+    except:
+        print("Pon el número, porfa")
+
+    return editores[genero_juego]
+
+
+def lista_editores_input():
+    editor = elegir_editores()
+    print("Has elegido a la empresa", editor)
+    print("--------------------------------------")
+    lista_juegos_editores(editor)
+
+
+def lista_juegos_editores(editor):
+    lista_juegos = buscar_varios_dicc(lista, "Publisher", editor)
+    lista_juego_2(lista_juegos)
+
+
+"""
+class Test_lista_juego_genero(unittest.TestCase):
+    
+    @patch('builtins.input')
+    def test_lista_juegos_genero(self, mock_input):
+        GENERO_A_PROBAR = "Sports"
+        lista_por_genero = buscar_varios_dicc(lista, "Genre", GENERO_A_PROBAR)
+        self.assertEqual(lista_por_genero[0]["Genre"],GENERO_A_PROBAR)
+        
+if __name__ == "__main__":
+    unittest.main()
+    
+"""
--- main.py
+++ main.py
@@ -15,42 +15,66 @@ def menu_opciones():
         except:
             print("Solo acepto las opciones que te di")
 
-        try:
-            match eleccion:
-                case 1:
-                    os.system('clear')
-                    alta_juego_input()
-                case 2:
-                    os.system('clear')
-                    borrar_juego()
-                case 3:
-                    os.system('clear')
-                    modificar_juego_input()
-                case 4:
-                    os.system('clear')
-                    listar_juego()
-                case 5:
-                    os.system('clear')
-                    buscar_por_nombre()
-                case 13:
-                    os.system('clear')
-                    print("Adios")
-                    permanecer = False
-                case _:
-                    os.system('clear')
-                    print("No se que has seleccionado")
+        match eleccion:
+            case 1:
+                os.system('clear')
+                alta_juego_input()
+            case 2:
+                os.system('clear')
+                borrar_juego()
+            case 3:
+                os.system('clear')
+                modificar_juego_input()
+            case 4:
+                os.system('clear')
+                listar_juego()
+            case 5:
+                os.system('clear')
+                buscar_por_nombre()
+            case 6:
+                os.system('clear')
+                mas_vendidos_mundo()
+            case 7:
+                os.system('clear')
+                lista_editores_input()
+            case 8:
+                os.system('clear')
+                lista_juegos_plataforma_input()
+            case 9:
+                os.system('clear')
+                juegos_siglo()
+            case 10:
+                os.system('clear')
+                lista_juegos_genero_input()
+            case 13:
+                os.system('clear')
+                print("Adios")
+                permanecer = False
+            case _:
+                os.system('clear')
+                print("No se que has seleccionado")
+
+
+""""
         except:
             os.system('clear')
             print("No se que has seleccionado, solo acepto las siguientes opciones:")
+    """
 
 
 def mostrar_opciones():
+    print()
     print("Menu")
     print(" 1 - Insertar un videojuego")
     print(" 2 - Borrar un videojuego")
     print(" 3 - Modificar un videojuego")
     print(" 4 - Listar los videojuegos")
     print(" 5 - Buscar por nombre")
+    print(" 6 - Lista de los juegos más vendidos en el mundo")
+    print(" 7 - Buscar por listado de los editores")
+    print(" 8 - Buscar por plataforma")
+    print(" 9 - Listado de juegos del siglo XX")
+    print(" 10 - Buscar por género")
     print(" 13 - Salir del programa")
     print("--------------------------------")
 
